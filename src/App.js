4ea2ea4bab81643da7ce0d05d27ab43c1ef46171
Nguyễn Xuân Ngọc <baitaptoan53@gmail.com>
2022-06-12 16:19:44 +0700

    update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,23 @@ import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 import "./App.css";
 
 const App = () => {
-  return <h1 className="underline text-3xl"> App</h1>;
+  <div>
+    <BrowserRouter>
+      <div className="flex relative dark:bg-main-dark-bg">
+        <div
+          className="fixer right-4 bottom-4 "
+          style={{ zIndex: "100" }}
+        ></div>
+        <TooltipComponent content="Settings" position="Top">
+          <button
+            type="button"
+            className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
+          >
+            <FiSettings></FiSettings>
+          </button>
+        </TooltipComponent>
+      </div>
+    </BrowserRouter>
+  </div>;
 };
 export default App;
